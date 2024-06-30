65ef234c26ad427c71ee22404bebcd2bf0e37233
Narendra <narendrach34@gmail.com>
2024-06-30 18:48:50 +0530

    added backend back

---
+++ backend/src/config/config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+
+module.exports = {
+  port: process.env.PORT_NO,
+  mongoose: {
+    url: process.env.MONGODB_URL,
+    options: {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+};
